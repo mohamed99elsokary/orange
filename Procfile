900c0cab8d054e86987e876bc4886bdda7d473f9
mohamed99elsokary <mohamed99elsokary@gmail.com>
2023-03-08 19:58:08 +0200

    ..

--- Procfile
+++ Procfile
@@ -1,3 +1,1 @@
-web: pip install django-import-export
-web: pip install -r requirements.txt
 web: python manage.py migrate && gunicorn mysite.wsgi
--- mysite/settings.py
+++ mysite/settings.py
@@ -32,6 +32,7 @@ CSRF_TRUSTED_ORIGINS = ["https://django-server-production-061e.up.railway.app"]
 # Application definition
 
 INSTALLED_APPS = [
+    "unfold",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -39,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "management",
+    "import_export",
 ]
 
 MIDDLEWARE = [
